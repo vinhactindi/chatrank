449f9d5bdabdb46fd89a60c7f43ce5b99460c539
vinhactindi <nguyen.the.vinh@actindi.net>
2022-01-26 11:02:19 +0700

    年月でランキング一覧を表示

--- app/controllers/servers/channels_controller.rb
+++ app/controllers/servers/channels_controller.rb
@@ -2,10 +2,18 @@
 
 class Servers::ChannelsController < ApplicationController
   before_action :set_server, only: %i[index create show]
+  before_action :set_period, only: %i[index show]
 
   def index
     @channels = Channel.includes(:children).where(server: @server).where(parent_id: nil)
-    @ranks = Rank.monthly(rankable_type: 'Server', rankable_id: @server.id)
+    @ranks = Rank.monthly(rankable_type: 'Server', rankable_id: @server.id, period: @period)
+  end
+
+  def show
+    @channel = Channel.find(params[:id])
+    redirect_to server_channels_path(@server.id), notice: 'チャットチャネルじゃありません' unless @channel.channel_type.zero?
+
+    @rank = Rank.monthly(rankable_type: 'Channel', rankable_id: @channel.id, period: @period)
   end
 
   def create
@@ -18,16 +26,14 @@ class Servers::ChannelsController < ApplicationController
     redirect_to server_channels_path(@server.id), notice: 'サーバーリストの更新が成功しました'
   end
 
-  def show
-    @channel = Channel.find(params[:id])
-    redirect_to server_channels_path(@server.id), notice: 'チャットチャネルじゃありません' unless @channel.channel_type.zero?
-
-    @rank = Rank.monthly(rankable_type: 'Channel', rankable_id: @channel.id)
-  end
-
   private
 
   def set_server
     @server = Server.find(params[:server_id])
+  end
+
+  def set_period
+    @period  = params[:period] || Time.current.strftime('%Y-%m')
+    @periods = Rank.distinct.pluck(:period)
   end
 end
