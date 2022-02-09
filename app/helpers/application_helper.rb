86d7bbd1ea5dac4e044332fc6fe54eac4adcd5a5
vinhactindi <nguyen.the.vinh@actindi.net>
2022-02-09 10:56:15 +0700

    menuのデザインを修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def invite_bot_url
+    "https://discord.com/api/oauth2/authorize?client_id=#{ENV['DISCORD_CLIENT_ID']}&permissions=68608&scope=bot"
+  end
 end
