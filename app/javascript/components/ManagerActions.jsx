ea290cc808f27d57f44f113a097c805156bba8b5
vinhactindi <nguyen.the.vinh@actindi.net>
2022-02-16 16:43:58 +0700

    Refreshの追加

--- app/javascript/components/ManagerActions.jsx
+++ app/javascript/components/ManagerActions.jsx
@@ -1,25 +1,36 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const ManagerActions = ({ isManager, server }) => {
-  if (!isManager) return null
-
+const ManagerActions = ({ isManager, server, onRefresh }) => {
   return (
     <div className="d-flex justify-content-between align-items-center mt-2">
-      あなたはこのサーバーの管理者です
-      <a
-        href={`/servers/${server.id}`}
-        role="button"
-        className="btn btn-link p-0 text-decoration-none">
-        過去統計
-      </a>
+      <span>
+        {isManager && (
+          <>
+            管理者ので、
+            <a
+              href={`/servers/${server.id}`}
+              role="button"
+              className="text-decoration-none">
+              過去統計
+            </a>
+            できます
+          </>
+        )}
+      </span>
+      <button
+        className="btn btn-link btn-sm p-0 text-decoration-none"
+        onClick={onRefresh}>
+        リフレッシュ
+      </button>
     </div>
   )
 }
 
 ManagerActions.propTypes = {
   server: PropTypes.object,
-  isManager: PropTypes.bool
+  isManager: PropTypes.bool,
+  onRefresh: PropTypes.func
 }
 
 export default ManagerActions
