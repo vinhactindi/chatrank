7ae4a1250e24f3edad9b63af064f4a2808e457df
vinhactindi <nguyen.the.vinh@actindi.net>
2022-02-21 15:50:05 +0700

    Ranksのテスト

--- test/system/ranks_test.rb
+++ test/system/ranks_test.rb
@@ -8,4 +8,18 @@ class RanksTest < ApplicationSystemTestCase
     visit root_url
     assert_text '過去統計'
   end
+
+  test 'visiting bootcamp leaderboard' do
+    login_user users(:vinh)
+    visit root_url
+
+    find('#server-select').click
+    find('#react-select-2-listbox').find('span', text: 'bootcamp').click
+
+    find('#period-select').click
+    find('#react-select-4-listbox').first('div', text: '2022-01').click
+
+    assert_text 'vinh#1111'
+    assert_text '10'
+  end
 end
--- test/system/servers_test.rb
+++ test/system/servers_test.rb
@@ -15,4 +15,14 @@ class ServersTest < ApplicationSystemTestCase
     visit server_path(servers(:shiba))
     assert_text 'あなたはこのサーバーの管理者ではありません'
   end
+
+  test 'reading history messages' do
+    login_user users(:vinh)
+
+    visit server_path(servers(:bootcamp))
+    click_link '過去統計'
+    accept_confirm
+
+    assert_text '過去のメッセージを読み込んでいますので、後で戻ってください'
+  end
 end
